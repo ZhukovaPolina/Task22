eab027ae5777d1ef0aed85eeb5b131f09579ff08
ZhukovaPolina <aleksandr.rds@gmail.com>
2025-12-16 10:00:33 +0300

    Update Lawyer.cpp

--- Library/Lawyer.cpp
+++ Library/Lawyer.cpp
@@ -1,13 +1,19 @@
-﻿#include "Lawyer.h"
+#include "Lawyer.h"
 #include "Case.h"
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 Lawyer::Lawyer(const std::string& name, const std::string& specialization, double rate)
     : Person(name), specialization(specialization), isAvailable(true), hourlyRate(rate) {}
 
 std::string Lawyer::getInfo() const {
-    return "Адвокат " + getFullName() + ", специализация: " + specialization +
-        ", ставка: " + std::to_string(hourlyRate) + " руб/час";
+    std::ostringstream oss;
+    oss << "Адвокат " << getFullName() 
+        << ", специализация: " << specialization 
+        << ", ставка: " << std::fixed << std::setprecision(2) << hourlyRate << " руб/час"
+        << ", дел: " << getCaseCount();
+    return oss.str();
 }
 
 std::string Lawyer::getRole() const {
@@ -26,21 +32,14 @@ double Lawyer::getHourlyRate() const {
     return hourlyRate;
 }
 
-int Lawyer::getCaseCount() const {
-    return cases.size();
-}
-
 void Lawyer::assignCase(Case* casePtr) {
-    cases.push_back(casePtr);
+    addCase(casePtr);
     isAvailable = false;
 }
 
-void Lawyer::removeCase(Case* casePtr) {
-    auto it = std::find(cases.begin(), cases.end(), casePtr);
-    if (it != cases.end()) {
-        cases.erase(it);
-    }
-    isAvailable = cases.empty();
+void Lawyer::completeCase(Case* casePtr) {
+    removeCase(casePtr);
+    isAvailable = getCaseCount() == 0;
 }
 
 void Lawyer::setAvailability(bool available) {

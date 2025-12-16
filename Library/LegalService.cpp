63ea8f841dc9c4d3853578a02444dd44aa25e558
ZhukovaPolina <aleksandr.rds@gmail.com>
2025-12-16 10:02:38 +0300

    Update LegalService.cpp

--- Library/LegalService.cpp
+++ Library/LegalService.cpp
@@ -1,7 +1,9 @@
-﻿#include "LegalService.h"
+#include "LegalService.h"
+#include "Case.h"
+#include <algorithm>
 
 LegalService::LegalService(const std::string& serviceName, double servicePrice,
-    const std::string& serviceCategory)
+                           const std::string& serviceCategory)
     : name(serviceName), price(servicePrice), category(serviceCategory) {}
 
 std::string LegalService::getName() const {
@@ -14,6 +16,25 @@ double LegalService::getPrice() const {
 
 std::string LegalService::getCategory() const {
     return category;
+}
+
+void LegalService::addCase(Case* casePtr) {
+    cases.push_back(casePtr);
+}
+
+void LegalService::removeCase(Case* casePtr) {
+    auto it = std::find(cases.begin(), cases.end(), casePtr);
+    if (it != cases.end()) {
+        cases.erase(it);
+    }
+}
+
+int LegalService::getCaseCount() const {
+    return static_cast<int>(cases.size());
+}
+
+const std::vector<Case*>& LegalService::getCases() const {
+    return cases;
 }
 
 void LegalService::setPrice(double newPrice) {

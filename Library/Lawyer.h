8d6bc10306a27a0086f4216f51c614353778b5e1
ZhukovaPolina <aleksandr.rds@gmail.com>
2025-12-16 10:00:04 +0300

    Update Lawyer.h

--- Library/Lawyer.h
+++ Library/Lawyer.h
@@ -3,31 +3,25 @@
 
 #include "Person.h"
 #include <string>
-#include <vector>
-
-class Case;
 
 class Lawyer : public Person {
 private:
     std::string specialization;
     bool isAvailable;
     double hourlyRate;
-
+    
 public:
     Lawyer(const std::string& name, const std::string& specialization, double rate);
-
     
     std::string getInfo() const override;
     std::string getRole() const override;
-
     
     std::string getSpecialization() const;
     bool getIsAvailable() const;
     double getHourlyRate() const;
-    int getCaseCount() const;
-
+    
     void assignCase(Case* casePtr);
-    void removeCase(Case* casePtr);
+    void completeCase(Case* casePtr);
     void setAvailability(bool available);
     void setHourlyRate(double rate);
 };

707ee1b00aa5002c8a3a95168b2c5b3d106a82ed
ZhukovaPolina <aleksandr.rds@gmail.com>
2025-12-16 09:59:24 +0300

    Update Person.cpp

--- Library/Person.cpp
+++ Library/Person.cpp
@@ -12,14 +12,30 @@ void Person::addCase(Case* casePtr) {
     cases.push_back(casePtr);
 }
 
+void Person::removeCase(Case* casePtr) {
+    auto it = std::find(cases.begin(), cases.end(), casePtr);
+    if (it != cases.end()) {
+        cases.erase(it);
+    }
+}
+
+int Person::getCaseCount() const {
+    return static_cast<int>(cases.size());
+}
+
+const std::vector<Case*>& Person::getCases() const {
+    return cases;
+}
+
 bool Person::involvedInCaseType(const std::string& caseType) const {
-    return std::any_of(cases.begin(), cases.end(), [&](const auto& casePtr) {
-        return casePtr->getServiceCategory() == caseType;
-        });
+    return std::any_of(cases.begin(), cases.end(), [&](Case* casePtr) {
+        return casePtr && casePtr->getServiceCategory() == caseType;
+    });
 }
 
 bool Person::involvedInCase(const std::string& caseType, int caseId) const {
-    return std::any_of(cases.begin(), cases.end(), [&](const auto& casePtr) {
-        return casePtr->getServiceCategory() == caseType && casePtr->getId() == caseId;
-        });
+    return std::any_of(cases.begin(), cases.end(), [&](Case* casePtr) {
+        return casePtr && casePtr->getServiceCategory() == caseType && 
+               casePtr->getId() == caseId;
+    });
 }

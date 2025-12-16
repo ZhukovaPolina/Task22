e6894062d419ff00e3651dc3a64795237535ecf4
ZhukovaPolina <aleksandr.rds@gmail.com>
2025-12-16 10:01:06 +0300

    Update Client.h

--- Library/Client.h
+++ Library/Client.h
@@ -5,25 +5,20 @@
 #include <string>
 #include <vector>
 
-class Case;
-
 class Client : public Person {
 private:
     std::string contactInfo;
-
+    
 public:
     Client(const std::string& name, const std::string& contact = "");
-
     
     std::string getInfo() const override;
     std::string getRole() const override;
-
     
     std::string getContactInfo() const;
     void setContactInfo(const std::string& contact);
-
+    
     std::vector<std::string> getServiceTypes() const;
-    int getCaseCount() const;
 };
 
 #endif
